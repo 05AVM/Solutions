aad653c3c1b46eb0ed75077a7de60a3614035dd1
Avishek <109818627+05AVM@users.noreply.github.com>
2024-01-25 23:44:17 +0530

    Time: 2 ms (63.20%), Space: 44.6 MB (47.92%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // solving it using dp and with TC-O(n) and SC-O(n)
-        int []max=new int[nums.length];
-        int []min=new int[nums.length];
+        // solving it using dp and with TC-O(n) with O(1) appraoch
+        //int []max=new int[nums.length];
+        //int []min=new int[nums.length];
         
-        Arrays.fill(max,0);
-        Arrays.fill(min,0);
+        //Arrays.fill(max,0);
+        //Arrays.fill(min,0);
         
-        max[0]=nums[0];
-        min[0]=nums[0];
+        int max=nums[0];
+        int min=nums[0];
+        int ans=nums[0];
         
         for(int i=1;i<nums.length;i++){
             int elem=nums[i];
             int ch1=elem;
             
-            int ch2=elem*max[i-1];
-            int ch3=elem*min[i-1];
+            int ch2=elem*max;
+            int ch3=elem*min;
             
-            max[i]=Math.max(Math.max(ch1,ch2),ch3);
-            min[i]=Math.min(Math.min(ch1,ch2),ch3);
+            max=Math.max(Math.max(ch1,ch2),ch3);
+            min=Math.min(Math.min(ch1,ch2),ch3);
+            ans=Math.max(ans,max);
         }
-        int result = max[0];
-        for (int i = 1; i < max.length; i++) {
-            result = Math.max(result, max[i]);
-        }
-
-        return result;
+        return ans;
+        
            
 }
 }
