ea66eb73aaed75f4b15a336a703630c26f8dda1b
Avishek <109818627+05AVM@users.noreply.github.com>
2024-02-27 18:42:43 +0530

    Time: 886 ms (78.35%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,4 @@
-select a.id as ID from Weather a, Weather b
-where a.recordDate-b.recordDate = 1 and
-a.temperature>b.temperature
-;
+SELECT a.id AS ID
+FROM Weather a
+JOIN Weather b ON a.recordDate = b.recordDate + INTERVAL '1' DAY
+WHERE a.temperature > b.temperature;
